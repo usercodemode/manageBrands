88e093ec9b01f2519e796dcda339a52c40cb719c
Codemode <codemodeuser@gmail.com>
2024-02-10 17:50:46 +0530

    Commit

--- function.php
+++ function.php
@@ -18,7 +18,7 @@ if (!empty($_POST['account']) && $_POST['account'] == "login" && !empty($_POST['
       $account = $DB->showData();
       $id = $account[0]['id'];
       $_SESSION['id'] = $id;
-      
+
       echo "success";
     } else {
       echo "Invalid credentials !";
@@ -46,7 +46,7 @@ if (!empty($_POST['account']) && $_POST['account'] == "register" && !empty($_POS
     if (count($DB->showData()) == 1) {
       echo "Email is already taken!";
       return;
-    } 
+    }
   }
 
   if ($DB->insert("insert into account(email, name, password) values(:email, :name, :password)", [':email' => $email, ':name' => $name, ':password' =>  md5($password)])) {
@@ -67,84 +67,90 @@ if (!empty($_POST['reload']) && $_POST['reload'] == "reload") {
 }
 
 if (!empty($_POST['upload']) && $_POST['upload'] == "brandLogo") {
-$valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp' , 'pdf' , 'doc' , 'ppt'); // valid extensions
-$path = 'uploads/'; // upload directory
-
-
-$img = $_FILES['file']['name'];
-$tmp = $_FILES['file']['tmp_name'];
-// get uploaded file's extension
-$ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
-// can upload same image using rand function
-$final_image = time().rand(1000,1000000).$img;
-// check's valid format
-if(in_array($ext, $valid_extensions)) 
-{ 
-$ImagePath = $path.md5($final_image).'.'.$ext; 
-if(move_uploaded_file($tmp,$ImagePath)) 
-{
-  $brandName = htmlspecialchars($_POST['brandName']);
-  $brandSite = htmlspecialchars($_POST['brandSite']);
-  
-  
-
-  if ($DB->insert("insert into brands(brandName, brandLogo, brandSite, user_id) values(:brandName, :brandLogo, :brandSite, :user_id)", [':brandName' => $brandName, ':brandLogo' => $ImagePath, ':brandSite' => $brandSite, ':user_id' => (int)$_SESSION["id"]])) {
-    echo "success";
-  } else {
-    echo 'Brand posting failed!';
-  }
-}
-} 
-else 
-{
-echo 'invalid';
-}
-}
-
-
-if (!empty($_POST['upload']) && $_POST['upload'] == "updateBrand") {
-  $valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp' , 'pdf' , 'doc' , 'ppt'); // valid extensions
+  $valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp', 'pdf', 'doc', 'ppt'); // valid extensions
   $path = 'uploads/'; // upload directory
+
+
   $img = $_FILES['file']['name'];
   $tmp = $_FILES['file']['tmp_name'];
   // get uploaded file's extension
   $ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
   // can upload same image using rand function
-  $final_image = time().rand(1000,1000000).$img;
+  $final_image = time() . rand(1000, 1000000) . $img;
   // check's valid format
-  if(in_array($ext, $valid_extensions)) 
-  { 
-  $ImagePath = $path.md5($final_image).'.'.$ext; 
-  if(move_uploaded_file($tmp,$ImagePath)) 
-  {
-    $brandName = htmlspecialchars($_POST['brandName']);
-    $brandSite = htmlspecialchars($_POST['brandSite']);
-   
-    $id = (int)htmlspecialchars($_POST['id']);
-  
-    if ($DB->update("UPDATE brands SET brandName=:brandName, brandLogo=:brandLogo, brandSite=:brandSite, user_id=:user_id 
-    WHERE id=:id", [':brandName' => $brandName, ':brandLogo' => $ImagePath, ':brandSite' => $brandSite, ':user_id' => (int)$_SESSION["id"], ':id' => $id])) {
-      echo "success";
-      //echo "Brand updated successfully!";
-      
-      if($DB->select("select brandLogo from brands where id=:id", [':id' => (int)$id])) {
-        $deleteBrandLogo = $DB->showData();
-        //unlink('/'.$deleteBrandLogo[0]["brandLogo"]);
-        unlink($deleteBrandLogo[0]["brandLogo"]);
+  if (in_array($ext, $valid_extensions)) {
+    $ImagePath = $path . md5($final_image) . '.' . $ext;
+    if (move_uploaded_file($tmp, $ImagePath)) {
+      $brandName = htmlspecialchars($_POST['brandName']);
+      $brandSite = htmlspecialchars($_POST['brandSite']);
+
+
+
+      if ($DB->insert("insert into brands(brandName, brandLogo, brandSite, user_id) values(:brandName, :brandLogo, :brandSite, :user_id)", [':brandName' => $brandName, ':brandLogo' => $ImagePath, ':brandSite' => $brandSite, ':user_id' => (int)$_SESSION["id"]])) {
+        echo "success";
+      } else {
+        echo 'Brand posting failed!';
       }
-
-    } else {
-      echo 'Brand update failed!';
     }
+  } else {
+    echo 'invalid';
   }
-  } 
-  else 
-  {
-  echo 'Something went wrong!';
-  }
+}
+
+
+if (!empty($_POST['upload']) && $_POST['upload'] == "updateBrand") {
+
+  $id = (int)htmlspecialchars($_POST['id']);
+
+  if ($DB->select("select brandLogo from brands where id=:id", [':id' => (int)$id])) {
+    $oldBrandLogo = $DB->showData();
   }
 
-  if (!empty($_POST['delete']) && $_POST['delete'] == "brand") {
+  if (!empty($_FILES['file']['name']) ) {
+    $valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp'); // valid extensions
+    $path = 'uploads/'; // upload directory
+    $img = $_FILES['file']['name'];
+    $tmp = $_FILES['file']['tmp_name'];
+    // get uploaded file's extension
+    $ext = strtolower(pathinfo($img, PATHINFO_EXTENSION));
+    // can upload same image using rand function
+    $final_image = time() . rand(1000, 1000000) . $img;
+    // check's valid format
+    if (in_array($ext, $valid_extensions)) {
+      $ImagePath = $path . md5($final_image) . '.' . $ext;
+      if (move_uploaded_file($tmp, $ImagePath)) {
+      
+      unlink($oldBrandLogo[0]["brandLogo"]);
+    }
+    }
+
+    //unlink('/'.$deleteBrandLogo[0]["brandLogo"]);
+    
+  } else {
+    $ImagePath = ($oldBrandLogo[0]["brandLogo"]);
+  }
+
+
+  $brandName = htmlspecialchars($_POST['brandName']);
+  $brandSite = htmlspecialchars($_POST['brandSite']);
+
+ 
+
+  if ($DB->update("UPDATE brands SET brandName=:brandName, brandLogo=:brandLogo, brandSite=:brandSite, user_id=:user_id 
+    WHERE id=:id", [':brandName' => $brandName, ':brandLogo' => $ImagePath, ':brandSite' => $brandSite, ':user_id' => (int)$_SESSION["id"], ':id' => $id])) {
+    echo "success";
+    //echo "Brand updated successfully!";
+
+
+  } else {
+    echo 'Brand update failed!';
+  }
+}
+
+
+
+
+if (!empty($_POST['delete']) && $_POST['delete'] == "brand") {
 
   $id = (int)htmlspecialchars($_POST['id']);
   if ($DB->delete("delete from brands where id=:id", [':id' => $id])) {
@@ -153,6 +159,3 @@ if (!empty($_POST['upload']) && $_POST['upload'] == "updateBrand") {
     echo "Brand deletion unsuccessfull!";
   }
 }
-
-
-?>
